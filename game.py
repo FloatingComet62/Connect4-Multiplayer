f82477520b7c798ccbd4c21628d7cd70a241a12a
FloatingComet62 <shubh2007rai@gmail.com>
2022-03-06 13:38:11 +0530

    Base GUI

---
+++ game.py
@@ -0,0 +1,72 @@
+import os
+
+blank = " "
+grid = [
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank]
+]
+
+def checkConnect4(list):
+    for row in list:
+        for i in range(len(row) - 1):
+            try:
+                item = row[i]
+                nextItem = row[i + 1]
+                nextnextItem = row[i + 2]
+                nextnextnextItem = row[i + 3]
+                if item == player and nextItem == player and nextnextItem == player and nextnextnextItem == player:
+                    return True
+            except:
+                pass
+
+def main(player, column):
+    grid.reverse()
+    for row in grid:
+        if row[column - 1] == blank:
+            row[column - 1] = player
+            break
+    grid.reverse()
+
+    columnList = [
+        [grid[0][0], grid[1][0], grid[2][0], grid[3][0], grid[4][0], grid[5][0], grid[6][0]],
+        [grid[0][1], grid[1][1], grid[2][1], grid[3][1], grid[4][1], grid[5][1], grid[6][1]],
+        [grid[0][2], grid[1][2], grid[2][2], grid[3][2], grid[4][2], grid[5][2], grid[6][2]],
+        [grid[0][3], grid[1][3], grid[2][3], grid[3][3], grid[4][3], grid[5][3], grid[6][3]],
+        [grid[0][4], grid[1][4], grid[2][4], grid[3][4], grid[4][4], grid[5][4], grid[6][4]],
+        [grid[0][5], grid[1][5], grid[2][5], grid[3][5], grid[4][5], grid[5][5], grid[6][5]],
+        [grid[0][6], grid[1][6], grid[2][6], grid[3][6], grid[4][6], grid[5][6], grid[6][6]]
+    ]
+    topLeftTobottomRight = [
+        [grid[3][0], grid[4][1], grid[5][2], grid[6][3]],
+        [grid[2][0], grid[3][1], grid[4][2], grid[5][3], grid[6][4]],
+        [grid[1][0], grid[2][1], grid[3][2], grid[4][3], grid[5][4], grid[6][5]],
+        [grid[0][0], grid[1][1], grid[2][2], grid[3][3], grid[4][4], grid[5][5], grid[6][6]],
+        [grid[0][1], grid[1][2], grid[2][3], grid[3][4], grid[4][5], grid[5][6]],
+        [grid[0][2], grid[1][3], grid[2][4], grid[3][5], grid[4][6]],
+        [grid[0][3], grid[1][4], grid[2][5], grid[3][6]],
+    ]
+    topRightTobottomLeft = [
+        [grid[0][3], grid[1][2], grid[2][1], grid[3][0]],
+        [grid[0][4], grid[1][3], grid[2][2], grid[3][1], grid[4][0]],
+        [grid[0][5], grid[1][4], grid[2][3], grid[3][2], grid[4][1], grid[5][0]],
+        [grid[0][6], grid[1][5], grid[2][4], grid[3][3], grid[4][2], grid[5][1], grid[6][0]],
+        [grid[5][0], grid[4][1], grid[3][2], grid[2][3], grid[1][4], grid[0][5]],
+        [grid[4][0], grid[3][1], grid[2][2], grid[1][3], grid[0][4]],
+        [grid[3][0], grid[2][1], grid[1][2], grid[0][3]],
+    ]
+
+    if checkConnect4(columnList) or checkConnect4(grid) or checkConnect4(topLeftTobottomRight) or checkConnect4(topRightTobottomLeft):
+        return {
+            "End": f"{player} wins!",
+            "Grid": grid
+        }
+    
+    return {
+        "End": "",
+        "Grid": grid
+    }
--- main.py
+++ main.py
@@ -1,89 +1,28 @@
-import os
+import tkinter
+import game
 
-blank = " "
-grid = [
-    [blank, blank, blank, blank, blank, blank, blank],
-    [blank, blank, blank, blank, blank, blank, blank],
-    [blank, blank, blank, blank, blank, blank, blank],
-    [blank, blank, blank, blank, blank, blank, blank],
-    [blank, blank, blank, blank, blank, blank, blank],
-    [blank, blank, blank, blank, blank, blank, blank],
-    [blank, blank, blank, blank, blank, blank, blank]
-]
-player = "X"
+window = tkinter.Tk()
+window.title("Connect 4")
+window.geometry("650x600")
 
-def checkConnect4(list):
-    for row in list:
-        for i in range(len(row) - 1):
-            try:
-                item = row[i]
-                nextItem = row[i + 1]
-                nextnextItem = row[i + 2]
-                nextnextnextItem = row[i + 3]
-                if item == player and nextItem == player and nextnextItem == player and nextnextnextItem == player:
-                    return True
-            except:
-                pass
+display = game.grid
 
-while True:
-    column = int(input("Which column?\n> "))
-    if column > 7 or column < 0:
-        print("Invalid column")
-        break
-    os.system("cls")
-    grid.reverse()
-    for row in grid:
-        if row[column - 1] == blank:
-            row[column - 1] = player
-            break
-    
-    grid.reverse()
+def sendColumn(column):
+    display = game.main("X", column)["Grid"]
 
-    columnList = [
-        [grid[0][0], grid[1][0], grid[2][0], grid[3][0], grid[4][0], grid[5][0], grid[6][0]],
-        [grid[0][1], grid[1][1], grid[2][1], grid[3][1], grid[4][1], grid[5][1], grid[6][1]],
-        [grid[0][2], grid[1][2], grid[2][2], grid[3][2], grid[4][2], grid[5][2], grid[6][2]],
-        [grid[0][3], grid[1][3], grid[2][3], grid[3][3], grid[4][3], grid[5][3], grid[6][3]],
-        [grid[0][4], grid[1][4], grid[2][4], grid[3][4], grid[4][4], grid[5][4], grid[6][4]],
-        [grid[0][5], grid[1][5], grid[2][5], grid[3][5], grid[4][5], grid[5][5], grid[6][5]],
-        [grid[0][6], grid[1][6], grid[2][6], grid[3][6], grid[4][6], grid[5][6], grid[6][6]]
-    ]
-    topLeftTobottomRight = [
-        [grid[3][0], grid[4][1], grid[5][2], grid[6][3]],
-        [grid[2][0], grid[3][1], grid[4][2], grid[5][3], grid[6][4]],
-        [grid[1][0], grid[2][1], grid[3][2], grid[4][3], grid[5][4], grid[6][5]],
-        [grid[0][0], grid[1][1], grid[2][2], grid[3][3], grid[4][4], grid[5][5], grid[6][6]],
-        [grid[0][1], grid[1][2], grid[2][3], grid[3][4], grid[4][5], grid[5][6]],
-        [grid[0][2], grid[1][3], grid[2][4], grid[3][5], grid[4][6]],
-        [grid[0][3], grid[1][4], grid[2][5], grid[3][6]],
-    ]
-    topRightTobottomLeft = [
-        [grid[0][3], grid[1][2], grid[2][1], grid[3][0]],
-        [grid[0][4], grid[1][3], grid[2][2], grid[3][1], grid[4][0]],
-        [grid[0][5], grid[1][4], grid[2][3], grid[3][2], grid[4][1], grid[5][0]],
-        [grid[0][6], grid[1][5], grid[2][4], grid[3][3], grid[4][2], grid[5][1], grid[6][0]],
-        [grid[5][0], grid[4][1], grid[3][2], grid[2][3], grid[1][4], grid[0][5]],
-        [grid[4][0], grid[3][1], grid[2][2], grid[1][3], grid[0][4]],
-        [grid[3][0], grid[2][1], grid[1][2], grid[0][3]],
-    ]
+chooseOne = tkinter.Button(window, text="1", bg="white", command=lambda: sendColumn(1))
+chooseOne.place(x=150, y=50, height=30, width=40)
+chooseTwo = tkinter.Button(window, text="2", bg="white", command=lambda: sendColumn(2))
+chooseTwo.place(x=200, y=50, height=30, width=40)
+chooseThree = tkinter.Button(window, text="3", bg="white", command=lambda: sendColumn(3))
+chooseThree.place(x=250, y=50, height=30, width=40)
+chooseFour = tkinter.Button(window, text="4", bg="white", command=lambda: sendColumn(4))
+chooseFour.place(x=300, y=50, height=30, width=40)
+chooseFive = tkinter.Button(window, text="5", bg="white", command=lambda: sendColumn(5))
+chooseFive.place(x=350, y=50, height=30, width=40)
+chooseSix = tkinter.Button(window, text="6", bg="white", command=lambda: sendColumn(6))
+chooseSix.place(x=400, y=50, height=30, width=40)
+chooseSeven = tkinter.Button(window, text="7", bg="white", command=lambda: sendColumn(7))
+chooseSeven.place(x=450, y=50, height=30, width=40)
 
-    if checkConnect4(columnList):
-        print(f"{player} wins!")
-        break
-    if checkConnect4(grid):
-        print(f"{player} wins!")
-        break
-    if checkConnect4(topLeftTobottomRight):
-        print(f"{player} wins!")
-        break
-    if checkConnect4(topRightTobottomLeft):
-        print(f"{player} wins!")
-        break
-    
-    if player == "X":
-        player = "O"
-    else:
-        player = "X"
-
-    for row in grid:
-        print(f"{row[0]} {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]}")
+window.mainloop()
